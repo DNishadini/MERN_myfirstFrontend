61aa134cbde0390e1999046d9a0f3ce9810ad716
DNishadini <dinushikaherath150@gmail.com>
2026-04-04 18:35:33 +0530

    create add product page

--- src/pages/admin/adminAddNewProduct.jsx
+++ src/pages/admin/adminAddNewProduct.jsx
@@ -1,3 +1,57 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 export default function AdminAddNewProduct() {
-  return <div>Sample add product page</div>;
+  const [productId, setProductId] = useState("");
+  const [name, setName] = useState("");
+  const [altNames, setAltNames] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [category, setCategory] = useState("cream");
+  const [images, setImages] = useState([]);
+  const [labelledPrice, setLabelledPrice] = useState(0);
+  const [stock, setStock] = useState(0);
+  const navigate = useNavigate();
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="w-[600px] h-[300px] border-accent border-[2px] flex flex-col items-center">
+        <input
+          value={productId}
+          onChange={(e) => setProductId(e.target.value)}
+        />
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <input value={altNames} onChange={(e) => setAltNames(e.target.value)} />
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <input
+          type="file"
+          onChange={(e) => {
+            setImages(e.target.files);
+          }}
+          multiple
+        />
+        <input
+          type="number"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <input
+          type="number"
+          value={labelledPrice}
+          onChange={(e) => setLabelledPrice(e.target.value)}
+        />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="cream">Cream</option>
+          <option value="lotion">Lotion</option>
+          <option value="serum">Serum</option>
+        </select>
+        <input
+          type="number"
+          value={stock}
+          onChange={(e) => setStock(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 }
---
+++ src/utils/mediaUpload.jsx
@@ -0,0 +1,32 @@
+import { createClient } from "@supabase/supabase-js";
+const anonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNla2h2eXhqYWJ2am9vem93c2drIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyNjE1NDksImV4cCI6MjA5MDgzNzU0OX0.0NbF1v00ZjvyByHXPl5XakoMLJmK3sBRGzeBgXfl24c";
+const supabaseUrl = "https://sekhvyxjabvjoozowsgk.supabase.co;";
+
+const supabase = createClient(supabaseUrl, anonKey);
+
+export default function MediaUpload(file) {
+  return new Promise((resolve, reject) => {
+    if (file == null) {
+      reject("No file provided");
+    } else {
+      const timestamp = new Date().getTime();
+      const fileName = timestamp + fileName;
+      supabase.storage
+        .from("images")
+        .upload(file.Name, file, {
+          upset: false,
+          cacheControl: "3600",
+        })
+        .then(() => {
+          const publicUrl = supabase.storage
+            .from("images")
+            .getPublicUrl(file.Name).data.publicUrl;
+          resolve(publicUrl);
+        })
+        .catch(() => {
+          reject("An error occured");
+        });
+    }
+  });
+}
